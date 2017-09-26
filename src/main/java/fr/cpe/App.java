d675fbcad281556ec7dccd0fcf9b7f2f98bc555d
robinwayne <robin.bertrand@yahoo.fr>
2017-09-26 10:58:17 +0200

    first commit

---
+++ src/main/java/fr/cpe/App.java
@@ -0,0 +1,17 @@
+package fr.cpe;
+
+
+import org.joda.time.DateTime;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        DateTime now = new DateTime();
+        System.out.println(now);
+    }
+}
